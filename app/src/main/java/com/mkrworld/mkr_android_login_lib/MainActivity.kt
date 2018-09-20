e3c4a61c3ec7c818bbd598a5ea49a262e46f0126
MKR-WORLD <manish.rewalliya@gmail.com>
2018-09-20 12:19:19 +0530

    first commit

---
+++ app/src/main/java/com/mkrworld/mkr_android_login_lib/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mkrworld.mkr_android_login_lib
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
